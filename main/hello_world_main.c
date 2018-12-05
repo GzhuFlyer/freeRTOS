77b67479a8fea4def91b73070311350dc6bcd8c9
gzhu_frank <gzhuflyer@foxmail.com>
2018-12-06 07:32:56 +0800

    .

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -1,62 +1,91 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_spi_flash.h"
-#include "esp_wifi.h"
+//#include "esp_system.h"
+//#include "esp_spi_flash.h"
+//#include "esp_wifi.h"
 
-void hex_str(unsigned char *inchar, unsigned int len, unsigned char *outtxt)
+#define DELAY_TIME 150000000
+
+static const char *pcTextForTask1 = "Task 1 is running \r\n";
+static const char *pcTextForTask2 = "Task 2 is running \r\n";
+static const char *pcTextForTask3 = "Task 3 is running \r\n";
+static const char *pcTextForTask4 = "Task 4 is running \r\n";
+
+
+void vPrintfString(char* c)
 {
-        unsigned char hbit,lbit;
-        unsigned int i;
-  for(i=0;i<len;i++)
-{
-        hbit = (*(inchar+i)&0xf0)>>4;
-      lbit = *(inchar+i)&0x0f;
-        if (hbit>9) outtxt[2*i]='A'+hbit-10;
-  else outtxt[2*i]='0'+hbit;
-        if (lbit>9) outtxt[2*i+1]='A'+lbit-10;
-  else    outtxt[2*i+1]='0'+lbit;
-    }
-outtxt[2*i] = 0;
+	printf("%s\n",c);
 }
+
+void vTask1(void *pvParameters)
+{
+//	const char *pvTaskName = "Task 1 is running \r\n";
+	volatile unsigned long u1;
+	for(;;)
+	{
+		vPrintfString(pvParameters);
+		vTaskDelay(500);
+//		for(u1 = 0;u1 < DELAY_TIME;u1++)
+//		{
+//
+//		}
+	}
+	vTaskDelete(NULL);
+}
+void vTask2(void *pvParameters)
+{
+//	const char *pvTaskName = "Task 2 is running \r\n";
+	volatile unsigned long u1;
+	for(;;)
+	{
+		vPrintfString(pvParameters);
+		vTaskDelay(500);
+//		for(u1 = 0;u1 < DELAY_TIME;u1++)
+//		{
+//
+//		}
+	}
+	vTaskDelete(NULL);
+}
+void vTask3(void *pvParameters)
+{
+//	const char *pvTaskName = "Task 3 is running \r\n";
+	volatile unsigned long u1;
+	for(;;)
+	{
+		vPrintfString(pvParameters);
+		vTaskDelay(500);
+//		for(u1 = 0;u1 < DELAY_TIME;u1++)
+//		{
+//
+//		}
+	}
+	vTaskDelete(NULL);
+}
+void vTask4(void *pvParameters)
+{
+//	const char *pvTaskName = "Task 4 is running \r\n";
+	volatile unsigned long u1;
+	for(;;)
+	{
+		vPrintfString(pvParameters);
+		vTaskDelay(500);
+//		for(u1 = 0;u1 < DELAY_TIME;u1++)
+//		{
+//
+//		}
+	}
+	vTaskDelete(NULL);
+}
+
+
 void app_main()
 {
     printf("Hello world!\n");
-	uint8_t eth_mac[6];
-	uint8_t mac[13];
-	mac[12]='\0';
-	esp_wifi_get_mac(WIFI_IF_STA, eth_mac);
-	for(int i=0;i<6;i++)
-		printf("%02x",eth_mac[i]);
-	hex_str(eth_mac,6,mac);
-	printf("the mac is %s\n",mac);
-//    /* Print chip information */
-//    esp_chip_info_t chip_info;
-//    esp_chip_info(&chip_info);
-//    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
-//            chip_info.cores,
-//            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-//            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-//
-//    printf("silicon revision %d, ", chip_info.revision);
-//
-//    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-//            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-//
-//    for (int i = 10; i >= 0; i--) {
-//        printf("Restarting in %d seconds...\n", i);
-//        vTaskDelay(1000 / portTICK_PERIOD_MS);
-//    }
-//    printf("Restarting now.\n");
-//    fflush(stdout);
-//    esp_restart();
+    xTaskCreate(vTask1,"Task 1",1000,pcTextForTask1,1,NULL);
+	xTaskCreate(vTask2,"Task 2",1000,pcTextForTask2,2,NULL);
+	xTaskCreate(vTask3,"Task 3",1000,pcTextForTask3,4,NULL);
+	xTaskCreate(vTask4,"Task 4",1000,pcTextForTask4,4,NULL);
+
 }
